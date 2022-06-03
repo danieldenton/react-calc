ccc3000747a09b3defe849a1b49ecb010170c607
danieldenton <danielmdenton@gmail.com>
2022-06-03 14:15:49 -0700

    rough beginning steppimng out

---
+++ src/Calculator.jsx
@@ -0,0 +1,24 @@
+export default function Calculator() {
+  return (
+    <div className="calculator">
+      <div className="calculator-display"></div>
+      <div className="keypad">
+        <button className="button">1</button>
+        <button className="button">2</button>
+        <button className="button">3</button>
+        <button className="button">4</button>
+        <button className="button">5</button>
+        <button className="button">6</button>
+        <button className="button">7</button>
+        <button className="button">8</button>
+        <button className="button">9</button>
+        <button className="button">0</button>
+        <button className="button">+</button>
+        <button className="button">-</button>
+        <button className="button">*</button>
+        <button className="button">/</button>
+        <button className="button">=</button>
+      </div>
+    </div>
+  );
+}
